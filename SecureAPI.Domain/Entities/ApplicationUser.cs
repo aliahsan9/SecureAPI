74c265860d19bfebc338c32d4c3a8e46d69bf378
Ali Ahsan <aliahsan8751@gmail.com>
2026-05-01 09:35:21 +0500

    added entities for applicaton user and refresh tokens

---
+++ SecureAPI.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureAPI.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastLoginAt { get; set; }
+
+        // Navigation properties
+        public ICollection<RefreshToken>? RefreshTokens { get; set; }
+
+
+    }
+}
---
+++ SecureAPI.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Builders;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureAPI.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string UserId { get; set; } = string.Empty;
+
+        // Navigation Property
+        public ApplicationUser? User { get; set; }
+
+
+
+    }
+}
